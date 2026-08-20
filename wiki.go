12e9b618c01bb789bbbb84ea9a668983d1ee3720
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-08-20 21:47:12 +0530

    docs: document reasoning

--- wiki.go
+++ wiki.go
@@ -9,14 +9,22 @@ import (
 
 type Page struct {
 	Title string
-	Body  []byte // a byte slice instead of a string slice because that is what the io libs expect
+	Body  []byte // a byte slice because files are fundamentally raw bytes, not strings
+	// []byte lets Go represent arbitrary file contents: text, images, binary, files, JSON, etc.
 }
 
+// save is a method instead of a plain function because it operates on a page, and it being a method makes the relationship clearer
+// if an operation belongs to a specific type, it is good to make it a method
+// no specific reason for using a pointer receiver instead of a value receiver, it isn't necessary here.
 func (p *Page) save() error { // since return type of WriteFile is error
 	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600) // 0600 = read write perms for the current user only
+	return os.WriteFile(filename, p.Body, 0600) // 0600 = read write perms for the current user only. The leading 0 means octal notation.
 }
 
+// loadPage is a function instead of a method because there isn't an existing Page to operate on yet
+// title -> find/load Page -> return Page
+// that is why it being a function makes sense
+// no specific reason for return *Page instead of just Page
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := os.ReadFile(filename) // ReadFile returns []byte and error
